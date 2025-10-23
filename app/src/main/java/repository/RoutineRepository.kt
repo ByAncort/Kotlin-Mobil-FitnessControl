685cf78fe10abcb9ac2615cccf03be4afc60b4fa
ByAncort <diego2005cortes@gmail.com>
2025-10-23 00:14:15 -0300

    repo: unify routine repository logic and improve data access

--- app/src/main/java/repository/RoutineRepository.kt
+++ app/src/main/java/repository/RoutineRepository.kt
@@ -3,26 +3,4 @@ package repository
 import data.Routine
 
 class RoutineRepository {
-
-    private val routines = mutableListOf<Routine>()
-    private var nextId = 1
-
-    fun saveRoutine(routine: Routine): Routine {
-        val newRoutine = routine.copy(id = nextId.toString())
-        routines.add(newRoutine)
-        nextId++
-        return newRoutine
-    }
-
-    fun getRoutines(): List<Routine> {
-        return routines.toList()
-    }
-
-    fun getRoutineById(id: String): Routine? {
-        return routines.find { it.id == id }
-    }
-
-    fun deleteRoutine(id: String): Boolean {
-        return routines.removeIf { it.id == id }
-    }
 }
