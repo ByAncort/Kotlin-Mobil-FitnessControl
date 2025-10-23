373c3b41348ce78d7995404941878ce5abe35ffa
ByAncort <diego2005cortes@gmail.com>
2025-10-23 00:14:29 -0300

    core: improve application initialization and dependency injection setup

--- app/src/main/java/com/example/fitnesscontrol/FitnessApplication.kt
+++ app/src/main/java/com/example/fitnesscontrol/FitnessApplication.kt
@@ -1,4 +1,11 @@
 package com.example.fitnesscontrol
 
-class FitnessApplication {
+import android.app.Application
+import data.AppDatabase
+import data.RoutineRepository
+
+class FitnessApplication : Application() {
+
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { RoutineRepository(database.routineDao()) }
 }
